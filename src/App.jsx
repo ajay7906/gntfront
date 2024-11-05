8eabe00e8d24774420f6ea654e44f7afd838e061
Ajay Kumar <aks969014@gmail.com>
2024-11-05 22:40:48 +0530

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Navbar from "./componets/Navbar/navbar";
 import Footer from "./componets/Footer/footer";
 
 //import Hero from "./pages/HeroSection/HeroSection"
-import AboutPage from "./pages/AboutPage/AboutUs";
-import  Contact  from "./pages/Contact/ContactUs";
+//import AboutPage from "./pages/AboutPage/AboutUs";
+// import  Contact  from "./pages/Contact/ContactUs";
+import ContactUs from './pages/Contact/ContactUs'
 import './styles.css';
 import Hero from './pages/hero/Hero';
+import AboutPage from "./pages/AboutPage/AboutUs"
 
 const App = () => {
   return (
@@ -15,8 +17,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Hero/>} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path="/contact" element={<ContactUs/>} />
       </Routes>
       <Footer/>
     </Router>
