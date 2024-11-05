8db2602dcca3b0a2ddd5f2630e93973d704e151f
Ajay Kumar <aks969014@gmail.com>
2024-11-05 22:44:00 +0530

    save changes in contact

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./componets/Footer/footer";
 //import Hero from "./pages/HeroSection/HeroSection"
 //import AboutPage from "./pages/AboutPage/AboutUs";
 // import  Contact  from "./pages/Contact/ContactUs";
+// import ContactUs from './pages/Contact/ContactUs'
 import ContactUs from './pages/Contact/ContactUs'
 import './styles.css';
 import Hero from './pages/hero/Hero';
