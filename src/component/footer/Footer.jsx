479587829baa7ccfa1d9de79b11b2444fe27dea1
Ajay Kumar <aks969014@gmail.com>
2024-12-03 18:37:18 +0530

    done

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -193,7 +193,65 @@ const Footer = () => {
                 </svg>
               </a>
             </div>
+
+
+
+
+
+            
+          <div className="mt-4 text-gray-300">
+                <div className="flex items-center mb-2">
+                  <svg className="h-5 w-5 mr-2 fill-current" viewBox="0 0 20 20">
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                  </svg>
+                  <a href="fin.hpcpl@gmail.com" className="hover:text-white">
+                  fin.hpcpl@gmail.com
+                  </a>
+                </div>
+                <div className="flex items-center">
+                  <svg className="h-5 w-5 mr-2 fill-current" viewBox="0 0 20 20">
+                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                  </svg>
+                  <a href="tel:+91 9784044883" className="hover:text-white">
+                  +91 9784044883
+                  </a>
+                </div>
+              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           </div>
+
+
+
+
+
+
+
+
         </div>
 
         {/* Bottom Section */}
