20c551d014e4c2ac3038ad69a8aa2955174a7759
Ajay Kumar <aks969014@gmail.com>
2025-01-17 13:12:53 +0530

    fix bug in main

--- src/marketing/TaskInformation.jsx
+++ src/marketing/TaskInformation.jsx
@@ -766,7 +766,7 @@ const TaskInformationForm = ({ taskId = '1', taskName = 'xyz' }) => {
                     <option value="">Select Category</option>
                     <option value="category1">Category 1</option>
                     <option value="category2">Category 2</option>
-                    <option value="category3">Category 3</option>
+                    <option value="category3">Category 3</option> 
                   </select>
                 </div>
 
